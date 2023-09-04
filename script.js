6f2876ed0a7d0d8250c92647b7de41a8dfae2a14
chrisdude94 <christerkodehode@gmail.com>
2023-09-04 14:23:57 +0200

    oppgave2

---
+++ script.js
@@ -0,0 +1,37 @@
+///oppgave 1.///
+
+const elever = ['Artur', 'Magnus', 'Audun', 'Desiree', 'Christer', 'Mari', 'Daniel', 'Ian', 'Bror', 'Isak'];
+console.log(elever[0]);
+console.log(elever [5]);
+
+elever.pop();
+
+console.log(elever);
+
+console.log(elever[5]);
+
+///oppgave 2.///
+
+const bosses = [{
+
+    firstName: "Ludwig",
+    lastName: "The Holy Blade",
+    age: 35,
+    
+}]
+
+const Ludwig = bosses[0]
+
+console.log(Ludwig.firstName);
+console.log(Ludwig.age);
+
+
+///oppgave 3.///
+
+
+
+///oppgave 4.///
+
+
+
+
